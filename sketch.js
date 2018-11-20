70a93f5da132dc01aa3b14ad847d3f2335c29830
terecrem <43389049+terecrem@users.noreply.github.com>
2018-11-20 10:50:29 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,71 @@
+var r=255
+var g= 0
+var b=0
+var myColor;
+var song;
+var canzone=false;
+var songs;
+
+
 function preload(){
-  // put preload code here
+  songs = [
+       {song: loadSound("assets/1.mp3"),},
+       {song: loadSound("assets/2.mp3"), },
+       {song: loadSound("assets/3.mp3"),},
+           ];
+ angleMode(DEGREES);
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight)
+  myColor = color(r,g,b)
+  background(myColor)
+
+   amp = new p5.Amplitude();
+
+    numSong = 0;
+   songs[numSong].song.play();
 }
 
 function draw() {
-  // put drawing code here
+
+
+    var vol = amp.getLevel();
+
+  myColor = color(r,g,b)
+  background(myColor)
+  if (r >5){
+    r-=5
+  }
+  if (g >5){
+    g-=5
+  }
+
+  fill('white');
+  textSize(30);
+  textStyle('bold')
+  textFont('gotham')
+  text("SHAKE TO CHANGE SONG", width/2, height/2);
+
+}
+
+function deviceTurned(){
+
+  if (turnAxis === 'X'){
+
+    r = 255
+  } else if (turnAxis === 'Z'){
+    g = 255
+  }
+}
+
+
+function deviceShaken(){
+    songs[numSong].song.stop();
+    numSong = numSong + 1;
+
+if (numSong >= songs.length) {
+            numSong = 0;
+        }
+    songs[numSong].song.play();
 }
